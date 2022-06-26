2808439e81f33ba9202b91e27c8917864558b1e6
aymanrgab <aymanrar2c@gmail.com>
2022-06-26 15:34:20 +0200

    add script to mount custom system and vendor images + move linked library files to odm folder

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+vendor_part=/data/.stowaways/vendor.img
+system_part=/data/.stowaways/system.img
+
+dmesg_info() {
+    echo "[mount-partitions.sh] $@" > /dev/kmsg
+}
+
+dmesg_info "Mount dynamic partitions"
+mkdir -p /system_root /vendor /system
+
+dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $system_part /system_root)"
+dmesg_info "$(mount --bind /system_root/system /system)"
+dmesg_info "$(mount -v -o loop,ro,barrier=1,discard -t ext4 $vendor_part /vendor)"
 
 if ! grep -q hybris /system/etc/ld.config.29.txt; then
     mount -o bind /usr/libexec/droid-hybris/system/etc/ld.config.29.txt /system/etc/ld.config.29.txt
 fi
-mount -o bind /system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.default.so
+
