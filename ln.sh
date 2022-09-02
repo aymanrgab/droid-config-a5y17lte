6d55e41a7b0a7c0db4cefe09b65cc99b72b5bfd3
aymanrgab <aymanrar2c@gmail.com>
2022-09-02 13:56:52 +0200

    fix symlinks  Please enter the commit message for your changes. Lines starting

--- ln.sh
+++ ln.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # link necessary init services
 files=(
-"a2dp_audio_policy_configuration.xml
-audio_policy_configuration.xml
-audio_policy_volumes.xml
-default_volume_tables.xml
-media_codecs.xml
-media_codecs_google_audio.xml
-media_codecs_google_telephony.xml
-media_codecs_google_video.xml
-media_profiles_V1_0.xml
-mixer_gains.xml
-mixer_paths.xml
-mixer_paths_0.xml
-r_submix_audio_policy_configuration.xml
-usb_audio_policy_configuration.xml"
+"a2dp_audio_policy_configuration.xml"
+"audio_policy_configuration.xml"
+"audio_policy_volumes.xml"
+"default_volume_tables.xml"
+"media_codecs.xml"
+"media_codecs_google_audio.xml"
+"media_codecs_google_telephony.xml"
+"media_codecs_google_video.xml"
+"media_profiles_V1_0.xml"
+"mixer_gains.xml"
+"mixer_paths.xml"
+"mixer_paths_0.xml"
+"r_submix_audio_policy_configuration.xml"
+"usb_audio_policy_configuration.xml"
 )
 
 for i in "${files[@]}"
